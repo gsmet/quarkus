5d392ee45a21fbda1ab21bdb6f01ad7e9ba01885
Guillaume Smet <guillaume.smet@gmail.com>
2021-03-31 12:51:51 +0200

    Simplify loading of Quarkus CLI version

--- devtools/cli/src/main/java/io/quarkus/cli/core/QuarkusCliVersion.java
+++ devtools/cli/src/main/java/io/quarkus/cli/core/QuarkusCliVersion.java
@@ -1,12 +1,8 @@
 package io.quarkus.cli.core;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
 import java.util.Properties;
 
-import io.smallrye.common.classloader.ClassPathUtils;
 import picocli.CommandLine;
 
 public class QuarkusCliVersion implements CommandLine.IVersionProvider {
@@ -17,18 +13,13 @@ public class QuarkusCliVersion implements CommandLine.IVersionProvider {
         if (version != null) {
             return version;
         }
-        final URL quarkusPropsUrl = Thread.currentThread().getContextClassLoader().getResource("quarkus.properties");
-        if (quarkusPropsUrl == null) {
-            throw new RuntimeException("Failed to locate quarkus.properties on the classpath");
-        }
+
         final Properties props = new Properties();
-        ClassPathUtils.consumeAsPath(quarkusPropsUrl, p -> {
-            try (BufferedReader reader = Files.newBufferedReader(p)) {
-                props.load(reader);
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to load quarkus.properties", e);
-            }
-        });
+        try {
+            props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("quarkus.properties"));
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to load quarkus.properties", e);
+        }
         version = props.getProperty("quarkus-core-version");
         if (version == null) {
             throw new RuntimeException("Failed to locate quarkus-core-version property in the bundled quarkus.properties");
