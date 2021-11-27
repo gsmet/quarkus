76dca7ebfa8c7795e7ad48a7c55f691bb9e5e3bf
Guillaume Smet <guillaume.smet@gmail.com>
2021-11-27 11:44:50 +0100

    Move the Amazon Services extensions to the Quarkiverse

--- core/deployment/src/main/java/io/quarkus/deployment/Feature.java
+++ core/deployment/src/main/java/io/quarkus/deployment/Feature.java
@@ -10,15 +10,7 @@ import io.quarkus.deployment.builditem.FeatureBuildItem;
 public enum Feature {
 
     AGROAL,
-    AMAZON_DYNAMODB,
     AMAZON_LAMBDA,
-    AMAZON_IAM,
-    AMAZON_S3,
-    AMAZON_SNS,
-    AMAZON_SQS,
-    AMAZON_SES,
-    AMAZON_KMS,
-    AMAZON_SSM,
     APICURIO_REGISTRY_AVRO,
     ARTEMIS_CORE,
     ARTEMIS_JMS,
